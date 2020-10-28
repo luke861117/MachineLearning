5e51b7118a6bc1c222fb5d66356645896366f3bf
Luke Chu <luke861117@gmail.com>
2020-10-28 10:49:10 +0800

    KNN
    Add KNN Method

---
+++ Arcanoid/KNN/knn_4_0_0.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 28 09:51:39 2020
+
+@author: DK
+"""
+
+
+import pickle
+import numpy as np
+
+import games.arkanoid.communication as comm
+from games.arkanoid.communication import ( SceneInfo, GameInstruction, GameStatus, PlatformAction)
+
+def ml_loop():    
+    filename="F:/Documents/MachineLearning/EX/Arkanoid-master/knn.sav"
+    # filename="F:/Documents/MachineLearning/MLGame-master/knn_test_2.sav"
+    model=pickle.load(open(filename, 'rb'))
+    comm.ml_ready()
+    ball_postition_history=[]    
+    while True:        
+       
+        scene_info = comm.get_scene_info()                
+        ball_postition_history.append(scene_info.ball)
+        
+        if(len(ball_postition_history) > 1):
+            vx=ball_postition_history[-1][0]-ball_postition_history[-2][0]
+            vy=ball_postition_history[-1][1]-ball_postition_history[-2][1]
+        
+            temp=np.array([scene_info.ball[0],scene_info.ball[1],scene_info.platform[0],vx,vy])
+            Result=temp[np.newaxis, :]
+        
+        # if scene_info.status == GameStatus.GAME_OVER or scene_info.status == GameStatus.GAME_PASS:           
+        #     print( "end" ,end='\n')            
+        #     comm.ml_ready()
+        #     continue
+        if scene_info.status == GameStatus.GAME_OVER:           
+            print( "Lose" ,end='\n')            
+            comm.ml_ready()
+            continue
+        elif scene_info.status == GameStatus.GAME_PASS:
+            print( "Win" ,end='\n')            
+            comm.ml_ready()
+            continue
+        
+        if(len(ball_postition_history) > 1):
+            Prediction=model.predict(Result)
+        else:
+            Prediction = 0        
+        if Prediction<0:
+            comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)            
+        elif Prediction>0:
+            comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
+        else:
+            comm.send_instruction(scene_info.frame, PlatformAction.NONE)
+        
---
+++ Arcanoid/Rulebase/README.md
@@ -0,0 +1,7 @@
+# Arkanoid 
+
+
+- rulebase_4.0_.py 2019.9.5 </p>
+
+- rulebase_8.0.1_.py 2020.10.21 </p>
+
